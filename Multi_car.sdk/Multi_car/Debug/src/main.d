8b0b63ddaa02b259ca85d9963390b2eee78fc4ce
xiefeifei <xiefei741000@outlook.com>
2019-06-01 15:54:35 +0800

    避障功能已完成，还需微调环境变量

---
+++ Multi_car.sdk/Multi_car/Debug/src/main.d
@@ -0,0 +1,67 @@
+src/main.o src/main.d: ../src/main.c \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xgpiops.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xgpiops_hw.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xuartps.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xuartps_hw.h ../src/macros.h \
+ ../src/cmd.h ../src/pwm.h \
+ ../../Multi_car_bsp/ps7_cortexa9_0/include/xil_printf.h ../src/ult.h
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xgpiops.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xgpiops_hw.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xuartps.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xuartps_hw.h:
+
+../src/macros.h:
+
+../src/cmd.h:
+
+../src/pwm.h:
+
+../../Multi_car_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../src/ult.h:
